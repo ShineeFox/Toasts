10cd103dbcbfc6b1cce87989fed60d0a26e2a0a0
ShineeFox <cbohm186@gmail.com>
2024-08-25 13:57:54 -0400

    Changed Class Names
    Changed class names from custom names to those of the tutorial so it would work properly.
    
    Toast pop ups were appearing and disappearing immediately, could not fid out why.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// script.js
+
 let icon = {
     success:
     '<span class="material-symbols-outlined">task_alt</span>',
@@ -9,62 +11,61 @@ let icon = {
     '<span class="material-symbols-outlined">info</span>',
 };
 
-// Creating the Toast Box Popup
-const showToast = ( message = "Sample Message", toastType = "info", duration = 5000) => {
-    if (!Object.keys(icon).includes(toastType))
+const showToast = (
+    message = "Sample Message",
+    toastType = "info",
+    duration = 5000) => {
+    if (
+        !Object.keys(icon).includes(toastType))
         toastType = "info";
 
     let box = document.createElement("div");
-
-    box.classList.add("toast", `toast-${toastType}`);
-
-    box.innerHTML = `   <div class="toast-content-wrapper">
-                        <div class="toast-icon">
-                        ${icon[toastType]}
-                        </div>
-                        <div class="toast-message">${message}</div>
-                        <div class="toast-progress"></div>
-                        </div>`;
-
+    box.classList.add(
+        "toast", `toast-${toastType}`);
+    box.innerHTML = ` <div class="toast-content-wrapper">
+                      <div class="toast-icon">
+                      ${icon[toastType]}
+                      </div>
+                      <div class="toast-message">${message}</div>
+                      <div class="toast-progress"></div>
+                      </div>`;
     duration = duration || 5000;
+    box.querySelector(".toast-progress").style.animationDuration =
+            `${duration / 1000}s`;
 
-    box.querySelector(".toast-progress").computedStyleMap.animationDuration = `${duration / 1000}s`;
-
-    let toastAlready = document.body.querySelector(".toast");
-
+    let toastAlready = 
+        document.body.querySelector(".toast");
     if (toastAlready) {
         toastAlready.remove();
     }
 
-    document.body.appendChild(box)
-};
+    document.body.appendChild(box)};
 
-// Setting the Correct Toasts for Event Listeners
-let submit = document.querySelector(".toast-level.perfect-toast");
+let submit = 
+    document.querySelector(".custom-toast.success-toast");
+let information = 
+    document.querySelector(".custom-toast.info-toast");
+let failed = 
+    document.querySelector(".custom-toast.danger-toast");
+let warn = 
+    document.querySelector(".custom-toast.warning-toast");
 
-let information = document.querySelector(".toast-level.toast-info");
+submit.addEventListener("click",(e) => {
+        e.preventDefault();
+        showToast("Bread Toasted to Perfection","success",5000);
+    });
 
-let failed = document.querySelector(".toast-level.burnt-toast");
+information.addEventListener("click",(e) => {
+        e.preventDefault();
+        showToast("Godzilla is the King of the Monsters","info",5000);
+    });
 
-let warning = document.querySelector(".toast-level.dark-toast");
+failed.addEventListener("click",(e) => {
+        e.preventDefault();
+        showToast("The Toast Was Burnt!","danger",5000);
+    });
 
-//Adding Event Listeners
-submit.addEventListener("click", (e) => {
+warn.addEventListener("click", (e) => {
     e.preventDefault();
-    showToast("Article Submitted Successfully", "success", 5000);
-});
-
-information.addEventListener("click", (e) => {
-    e.preventDefault();
-    showToast("Godzilla is the King of the Monsters", "info", 5000);
-});
-
-failed.addEventListener("click", (e) => {
-    e.preventDefault();
-    showToast("Failed! Unexpected Error!", "danger", 5000);
-});
-
-warning.addEventListener("click", (e) => {
-    e.preventDefault();
-    showToast("!Warning! Server Error", "warning", 5000);
+    showToast("!Warning! Toast is Burning","warning",5000);
 });
